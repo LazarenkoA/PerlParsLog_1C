f040e0af1ae159775927df9842f7bb54558992e5
Artem <laz-mail@mail.ru>
2018-10-03 08:57:05 +0300

    улучшения (стек теперь полный выводится, актуально для многострочных стеков), так же считаются различные события

--- CallDurationsMem.pl
+++ CallDurationsMem.pl
@@ -1,108 +1,158 @@
-#!/usr/bin/perl
+﻿#!/usr/bin/perl
 use strict;
-#use Encode;
+use warnings;
+use File::Find;
+use Cwd;
+use utf8;
 use Data::Dumper;   
 use Time::HiRes qw(gettimeofday tv_interval);
 #use encoding 'cp1251';
-#use Encode::Locale;
+use Encode;
 #use autodie;  # automatic error handling  
 use Benchmark; # Для замера выполнения кода
 use XML::Simple;
+use Digest::MD5 qw(md5 md5_hex);
 use 5.016;
 
+binmode(STDOUT,':utf8');
 
-my %Hash;
+my %HashValue;
+my %HashName;
 my $top;
 my $SortByMem = 0;
+my $SortByCount = 0;
 my $GroupByDB = 0;
 my $SortByOneCall = 0;
 my $start_time = Benchmark->new;
+my @directories_to_search = (getcwd); # Текущая директория.  
 InitializationParams();
 #Time::HiRes::usleep(100000);
 
-while (<STDIN>) {
-    ParsLine($_) if (/^\d\d:\d\d\.\d+(.+?),CALL/);
-} continue {
-    close ARGV if eof;  # Not eof()!
+find(\&wanted, @directories_to_search);
+sub wanted {
+    ParsFile($_) if /^[\w]+.log$/i;
 }
 
+#while (<STDIN>) {
+#    ParsLine($_) if (/^\d\d:\d\d\.\d+(.+?),CALL/);
+#} continue {
+#    close ARGV if eof;  # Not eof()!
+#}
 
-my $index = 1;
- # Выводим отсортированные (по убыванию) данные. Сортировка по значениею хеша
-foreach my $Key (sort sort_func keys %Hash) {
-    last if $top eq $index;
+sub ParsFile() {
+    my $fileName = shift;
+    open(my $FH, "<:encoding(utf8)", $fileName) or die "Ошибка открытия файла $fileName\n$!";
 
-    my $ValueKb = sprintf("%.2f", $Hash{$Key}{Value} / 1024);    
-    my $ValueMb = sprintf("%.2f", $Hash{$Key}{Value} / 1024**2);   
-    my $AvValueKb = sprintf("%.2f", ($Hash{$Key}{Value} / 1024) / $Hash{$Key}{Count});   
-    my $ValueSec = sprintf("%.2f", $Hash{$Key}{Value} / 1000000);   
-    my $AvValueSec = sprintf("%.2f", ($Hash{$Key}{Value} / 1000000) / $Hash{$Key}{Count});   
-    my $ValueMin = sprintf("%.2f", $Hash{$Key}{Value} / 1000000 / 60);   
+    my $txt;
+    {
+        local $/ = "\r";
+        $txt = <$FH>;
+    } 
+    close $FH;
 
-    # duration для 8.3 это миллионные доли секунды
-    if(not $SortByMem) {
-        print "$Key" . " - ~ $ValueSec сек., ~ $ValueMin мин. (вызов $Hash{$Key}{Count} раз, среднее значение за вызов $AvValueSec сек.)\n";
-    } else {
-        print "$Key" . " - ~ $ValueKb Kb., ~$ValueMb Mb. (вызов $Hash{$Key}{Count} раз, среднее значение за вызов $AvValueKb Kb.) \n";
+    my @events = ("CALL", "DBMSSQL");
+
+    local $" = "|";
+    foreach($txt =~ /(^\d\d:\d\d\.\d+[-]\d+,(?|@events)(?:.*?)(?|Method[^,]+|Context=([^'][^,]+)|Context='([^']+)).*?$)/gsm) {
+       ParsLine($_);
     }
-   $index++;
+}
+
+my $index = 0;
+ # Выводим отсортированные (по убыванию) данные. Сортировка по значениею хеша
+foreach my $Key (sort sort_func keys %HashValue) {
+    last if defined($top) and $top eq $index;
+
+    say "------------ (вызов $HashValue{$Key}{Count} раз) ------------";
+    foreach(keys %{$HashValue{$Key}{Value}}) {
+        my $ValueKb = sprintf("%.2f", $HashValue{$Key}{Value}{$_} / 1024);    
+        my $ValueMb = sprintf("%.2f", $HashValue{$Key}{Value}{$_} / 1024**2);   
+        my $AvValueKb = sprintf("%.2f", ($HashValue{$Key}{Value}{$_} / 1024) / $HashValue{$Key}{Count});   
+        my $ValueSec = sprintf("%.2f", $HashValue{$Key}{Value}{$_} / 1000000);   
+        my $AvValueSec = sprintf("%.2f", ($HashValue{$Key}{Value}{$_} / 1000000) / $HashValue{$Key}{Count});   
+        my $ValueMin = sprintf("%.2f", $HashValue{$Key}{Value}{$_} / 1000000 / 60);   
+
+        # duration для 8.3 это миллионные доли секунды
+        if(not $SortByMem) {
+            say "$_ [~ $ValueSec сек., ~ $ValueMin мин., среднее значение за вызов $AvValueSec сек.)]";
+        } else {
+            say "$_ [~ $ValueKb Kb., ~$ValueMb Mb., среднее значение за вызов $AvValueKb Kb.)]";
+        }
+    }
+
+    say "$HashName{$Key}\n";
+    
+   $index++; 
 }
 
 sub sort_func {
-    ($SortByOneCall and ($Hash{$b}{Value} / $Hash{$b}{Count}) <=> ($Hash{$a}{Value} / $Hash{$a}{Count}))
-    || (not $SortByOneCall and $Hash{$b}{Value} <=> $Hash{$a}{Value})
+    ($SortByOneCall and ($HashValue{$b}{Value}{Common} / $HashValue{$b}{Count}) <=> ($HashValue{$a}{Value}{Common} / $HashValue{$a}{Count}))
+    || ($SortByCount and $HashValue{$b}{Count} <=> $HashValue{$a}{Count})
+    || (not $SortByOneCall and not $SortByCount and $HashValue{$b}{Value}{Common} <=> $HashValue{$a}{Value}{Common}) # по дефолту
 }
 
 my $end_time = Benchmark->new;
 my $delta = timediff($end_time, $start_time);
-print "\n\nВремя выполнение скрипта:\n" . timestr($delta);
+say "\n\nВремя выполнение скрипта:\n" . timestr($delta);
 
 sub ParsLine() {
-    my ($line) = @_;
-    
-    #my %tmp = GetHashFromLine($line);
-    my %tmp = GetHashFromLine($line);
+    my $line = shift;
+
+    my %tmp = GetHashValueFromLine($line);
     
     while(my($k, $v) = each(%tmp)) {
-       $Hash{$k}{Count} += $$v{Count};
-       $Hash{$k}{Value} += $$v{Value};
-
-       #print "$$v{Value}\n";
+       $HashValue{$k}{Count} += $$v{Count};
+       map {
+           $HashValue{$k}{Value}{$_} += $$v{Value}{$_};
+           $HashValue{$k}{Value}{Common} += $$v{Value}{$_}
+       } keys %{$$v{Value}};
+       #$HashValue{$k}{Value} += $$v{Value};
     }
   }
 
-sub GetHashFromLine($) {
-    my ($line) = @_;
-   # my $Context;
-    my %Hash;
+sub GetHashValueFromLine($) {
+    my ($line) = shift;
+    return unless $line;
 
-    my $matching = $line =~ /p:processName=(?<DB>[^,]+)(.+?)Context=(?<Context>[^,]+)/;
+   # my $Context;
+    my %HashValue;
+
+# while($txt =~ /^\d\d:\d\d\.\d+[-]\d+,(?|@events)(?:.*?)(?|Method[^,]+|Context=([^'][^,]+)|Context='([^']+)).*?$/gm) {
+    my $matching = $line =~ /p:processName=(?<DB>[^,]+)(.+?)(?|Context=(?<Context>[^,]+)|Context='(?<Context>[^']+))/s;
     my($DB, $Context) = ($+{DB}, $+{Context}) if $matching; 
     ($DB, $Context) = ($+{DB}, "$+{Module}.$+{Method}") if !$matching and $line =~ /p:processName=(?<DB>[^,]+)(.+?)Module=(?<Module>[^,]+)(?:.+?)Method=(?<Method>[^,]+)/; # Если контекста нет берем имя модуля и метода
 
-    # Из контекста убираем пробелы и переносы строк + Context может быть обрамлен '', убираем и их
-    $Context =~ s/\n//g;
-    $Context =~ s/\s//g;
-    $Context =~ s/[']//g;
-
-    my $Key = "[$DB] $Context";
-    $Key = $DB if $GroupByDB;
     return unless $Context; # Выходим если контекста нет, накой нам эти строки.
 
-    my $Value = $+{Value} if (not $SortByMem and $line =~ /^[\d]+:[\d]+\.[\d]+[-](?<Value>[\d]+)/) or ($SortByMem and $line =~ /Memory=(?<Value>[-]?[\d]+)/); #MemoryPeak
-    $Hash{$Key} = {
+    my $KeySource = "[$DB] $Context";
+    $KeySource = $DB if $GroupByDB;
+    my $Key = getHash($KeySource);
+   
+   $HashName{$Key} = $KeySource;
+
+    my $Value = $+{Value} if (not $SortByMem and $line =~ /^[\d]+:[\d]+\.[\d]+[-](?<Value>[\d]+)[,](?<event>[^,]+)/) or ($SortByMem and $line =~ /Memory=(?<Value>[-]?[\d]+)/); #MemoryPeak
+    $HashValue{$Key} = {
         Count => 1,
-        Value => $Value
+        Value => {$+{event} => $Value}
     };
 
-    return %Hash;
+    return %HashValue;
   }
 
-  sub InitializationParams() {
+  sub InitializationParams {
       foreach (@ARGV) {
         $top = $1 if /top([\d]+)/;
         $SortByMem = uc($_) eq uc("SortByMem") if not $SortByMem;
         $SortByOneCall = uc($_) eq uc("SortByOneCall") if not $SortByOneCall;
         $GroupByDB = uc($_) eq uc("GroupByDB") if not $GroupByDB;
+        $SortByCount = uc($_) eq uc("SortByCount") if not $SortByCount;
       }
+  }
+
+  sub getHash() {
+      my $str = shift;
+
+    # Из контекста убираем все лишние
+    $str =~ s/[\n\s'\.;:\(\)\"\'\d]//g;
+    return md5_hex(encode_utf8($str));
   }
